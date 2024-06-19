ced9cc23105399a445988dec6feba990555080df
Sharul Ariff <sharul9@aol.com>
2024-06-20 01:29:03 +1000

    Initial Commit

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by sharul ariff on 20/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by sharul ariff on 20/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
